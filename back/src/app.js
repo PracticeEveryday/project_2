6133f4954f196db26f2ce7699e0151e76d4ddb31
DongHyun Kim <developerforus@gmail.com>
2022-04-23 11:25:30 +0900

    passport division

--- back/src/app.js
+++ back/src/app.js
@@ -6,47 +6,28 @@ import { loginRouter } from "./routers/loginRouter";
 import { CocktailRouter } from "./routers/CocktailRouter";
 import { RankRouter } from "./routers/RankRouter";
 
-import passport from "passport";
-import session from "express-session";
-import googleOAuth from "./utils/googleOAuth";
+import { passport } from "./passport/googlePassport";
+
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-passport.use(googleOAuth);
-
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-app.use(registerRouter);
-app.use(loginRouter);
-app.use(CocktailRouter);
-app.use(RankRouter);
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
 
 app.get("/", (req, res) => {
   res.send("안녕하세요, 레이서 프로젝트 API 입니다.");
 });
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/error" }),
-  function (req, res) {
-    res.redirect("/");
-  }
-);
+app.get("/auth/google/callback", passport.authenticate("google", { failureRedirect: "/error" }), function (req, res) {
+  res.redirect("/");
+});
 
 app.use(registerRouter);
 app.use(loginRouter);
+app.use(CocktailRouter);
+app.use(RankRouter);
 app.use(errorMiddleware);
-
 export { app };
