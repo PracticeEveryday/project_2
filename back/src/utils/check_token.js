1e91b6d78860153d5cfc3cb11f1ccc4fda39370b
DongHyun Kim <developerforus@gmail.com>
2022-04-24 13:39:57 +0900

    refreshtoken succ

--- back/src/utils/check_token.js
+++ back/src/utils/check_token.js
@@ -10,10 +10,9 @@ const jwtkey = process.env.JWT_KEY;
 const checkAccessToken = async accessToken => {
   try {
     const decoded = jwt.verify(accessToken, jwtkey);
-    accessToken = makeToken(decoded);
     return {
       status: "succ",
-      accessToken,
+      decoded: { decoded: decoded.userId },
     };
   } catch (error) {
     return {
